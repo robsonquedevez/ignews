92b0046dde0b992c91787f5ea5b1be27536cb174
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.com.br>
2022-02-17 14:19:03 -0300

    Implementation FaunaDB integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,27 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-    callbacks: {
-        session({ session, token, user}) {
-            return session;
-        }
-    },
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,            
-        })
+        }),
     ],
+    callbacks: {
+        async signIn({user, account, profile}) {
+            const { email } = user;
+            
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { email } }
+                )
+            )
+
+            return true;
+        }
+    }
 })
