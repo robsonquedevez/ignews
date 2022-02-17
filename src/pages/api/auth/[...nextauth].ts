6a9f2c40484751d8e50f9dfc154c7da110777986
Robson Quedevez <robson.quedevez@hotmail.com>
2022-02-17 20:54:22 -0300

    Verify exists user in database before save new login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,15 +13,35 @@ export default NextAuth({
     callbacks: {
         async signIn({user, account, profile}) {
             const { email } = user;
-            
-            await fauna.query(
-                q.Create(
-                    q.Collection('users'),
-                    { data: { email } }
-                )
-            )
 
-            return true;
+            try {
+                await fauna.query(
+                   
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }        
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )                        
+                    )
+                )
+                return true;
+            } catch (error) {
+                return false;
+            }
         }
     }
 })
