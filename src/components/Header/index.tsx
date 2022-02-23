c603d6e19a569cc8c95fbe08886cf1468bea8bf7
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.com.br>
2022-02-23 10:09:29 -0300

    Active menu header implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
-import { SignButton } from '../SignButton/indes'
-import styles from './styles.module.scss'
+import { SignButton } from '../SignButton';
+import { ActiveLink } from '../ActiveLink'
+import styles from './styles.module.scss';
 
-export function Header() {
+export function Header() {   
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignButton />
             </div>
